95d824b7175596a56176e6f9b74f31f6119be82e
Achaak <achaak@users.noreply.github.com>
2018-05-30 11:44:06 +0200

    Add file Halls of Element

---
+++ model/hof.js
@@ -0,0 +1,26 @@
+/* HOF10 */
+    /* SMALL CRYSTAL */
+    /* END SMALL CRYSTAL */
+
+    /* BIG CRYSTAL */
+    /* END BIG CRYSTAL */
+
+    /* CRYSTAL WAVE 3 */
+    /* END CRYSTAL WAVE 3 */
+
+    /* L. CRYSTAL WAVE 5 */
+    /* END L. CRYSTAL WAVE 5 */
+
+    /* R. CRYSTAL WAVE 5 */
+    /* END R. CRYSTAL WAVE 5 */
+
+
+    /* MINI BOSS */
+    /* END MINI BOSS */
+
+    /* BOSS */
+    /* END BOSS */
+/* END HOF10 */
+
+var hof10 = [
+];
