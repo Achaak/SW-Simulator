2ec1994d1b1b7dca38d13281c8c714079dbeabd9
Achaak <achaak@users.noreply.github.com>
2018-05-30 11:37:44 +0200

    Add hall of element

---
+++ controller/howi.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <!--<base href="/">-->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+    <title>Title</title>
+    <meta name="description" content="Description" />
+    <meta property="og:title" content="Title" />
+    <meta property="og:description" content="Description" />
+    <meta property="og:image" content="" />
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS -->
+        <link type="text/css" rel="stylesheet" href="view/css/common.css">
+
+        <link type="text/css" rel="stylesheet" href="framework/flexbox/flexboxgrid.min.css">
+        <link type="text/css" rel="stylesheet" href="framework/jquery-ui/jquery-ui.min.css">
+        <link type="text/css" rel="stylesheet" href="framework/loading-bar/loading-bar.css"/>
+    <!-- END CSS -->
+
+    <link rel="icon" href="">
+</head>
+<body>
+    <!-- HEADER -->
+        <header>
+
+        </header>
+    <!-- END HEADER -->
+    
+    
+
+    <!-- SECTION -->
+        <section>    
+            <div class=row>  
+                <?php include_once('../view/import/nav.php'); ?>
+
+                <?php include_once('../view/import/interface-simulate.php'); ?>
+            </div>
+        </section>
+    <!-- END SECTION -->
+
+
+    
+    <!-- FOOTER -->
+        <footer>
+
+        </footer>
+    <!-- END FOOTER -->
+
+
+
+    <!-- JAVASCRIPT -->
+        <script src="framework/jquery/jquery.min.js"></script>
+        <script src="framework/jquery-ui/jquery-ui.min.js"></script>
+        <script src="framework/loading-bar/loading-bar.js"></script>
+
+        <script src="js/nav.js"></script>
+
+        <?php include_once('../view/import/js-simulator.php'); ?>
+        <script src="model/howi.js"></script>
+        
+        <script type="text/javascript">
+            $(document).ready(function(){     
+                classEnnemiesWaves = howi10;
+            });
+        </script>
+    <!-- END JAVASCRIPT -->
+</body>
+</html>
--- view/import/nav.php
+++ view/import/nav.php
@@ -6,9 +6,17 @@
     </ul>
 
     <ul class='menu'>
-        <li><a href='import'><img src='view/pictures/icons/icon-gb.png'><span class='li-name'>Import</span></a></li>
-        <li><a href='giant'><img src='view/pictures/icons/icon-gb.png'><span class='li-name'>Giant</span></a></li>
-        <li><a href='dragon'><img src='view/pictures/icons/icon-gb.png'><span class='li-name'>Dragon</span></a></li>
-        <li><a href='lich'><img src='view/pictures/icons/icon-gb.png'><span class='li-name'>Lich</span></a></li>
+        <li><a href='Import'><img src='view/pictures/icons/icon-gb.png'><span class='li-name'>Import</span></a></li>
+        <li><a href='Giant'><img src='view/pictures/icons/icon-gb.png'><span class='li-name'>Giant</span></a></li>
+        <li><a href='Dragon'><img src='view/pictures/icons/icon-gb.png'><span class='li-name'>Dragon</span></a></li>
+        <li><a href='Lich'><img src='view/pictures/icons/icon-gb.png'><span class='li-name'>Lich</span></a></li>
+
+        <li><a href='HallOfMagic'><img src='view/pictures/icons/icon-gb.png'><span class='li-name'>Hall Of Magic</span></a></li>
+
+        <li><a href='HallOfWater'><img src='view/pictures/icons/icon-gb.png'><span class='li-name'>Hall Of Water</span></a></li>
+        <li><a href='HallOfFire'><img src='view/pictures/icons/icon-gb.png'><span class='li-name'>Hall Of Fire</span></a></li>
+        <li><a href='HallOfWind'><img src='view/pictures/icons/icon-gb.png'><span class='li-name'>Hall Of Wind</span></a></li>
+        <li><a href='HallOfLight'><img src='view/pictures/icons/icon-gb.png'><span class='li-name'>Hall Of Light</span></a></li>
+        <li><a href='HallOfDark'><img src='view/pictures/icons/icon-gb.png'><span class='li-name'>Hall Of Dark</span></a></li>
     </ul>
 </nav>
